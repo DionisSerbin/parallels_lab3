ac7daba940a5ce54f9de11278490634cd38b060b
DionisSerbin <reaganusm@gmail.com>
2021-11-04 16:29:16 +0300

    Scripted auto-commit on change (2021-11-04 16:29:16) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StatisticDelay.java
+++ src/main/java/ru/bmstu/iu9/StatisticDelay.java
@@ -14,7 +14,54 @@ public class StatisticDelay  implements Serializable {
     public StatisticDelay(int countAllFlights, int countCancelledFlights,
                           int countDelayedFlights, float maxDelayed){
 
-        
+        this.countAllFlights = countAllFlights;
+        this.countCancelledFlights = countCancelledFlights;
+        this.countDelayedFlights = countDelayedFlights;
+        this.maxDelayed = maxDelayed;
+    }
 
+    public int getCountAllFlights(){
+        return countAllFlights;
+    }
+
+    public int getCountCancelledFlights(){
+        return countCancelledFlights;
+    }
+
+    public int getCountDelayedFlights(){
+        return countDelayedFlights;
+    }
+
+    public float getMaxDelayed(){
+        return maxDelayed;
+    }
+
+    public void setCountAllFlights(int countAllFlights) {
+        this.countAllFlights = countAllFlights;
+    }
+
+    public void setCountCancelledFlights(int countCancelledFlights) {
+        this.countCancelledFlights = countCancelledFlights;
+    }
+
+    public void setCountDelayedFlights(int countDelayedFlights) {
+        this.countDelayedFlights = countDelayedFlights;
+    }
+
+    public void setMaxDelayed(float maxDelayed) {
+        this.maxDelayed = maxDelayed;
+    }
+
+    public static addStatistic(StatisticDelay )
+
+    @Override
+    public String toString() {
+        return
+                "StatisticDelay{" +
+                        "countAllFlights=" + countAllFlights +
+                        ", countCancelledFlights=" + countCancelledFlights +
+                        ", countDelayedFlights=" + countDelayedFlights +
+                        ", maxDelayed=" + maxDelayed +
+                        '}';
     }
 }
