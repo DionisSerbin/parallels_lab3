f6a3a5b97a15232e6ae820df06234fba3d703a48
DionisSerbin <reaganusm@gmail.com>
2021-11-04 14:16:11 +0300

    Scripted auto-commit on change (2021-11-04 14:16:11) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/SparkApp.java
+++ src/main/java/ru/bmstu/iu9/SparkApp.java
@@ -4,10 +4,17 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import scala.Tuple2;
+
+import java.util.Map;
 
 public class SparkApp {
 
-    private static final String 
+    private static final String CODE = "Code";
+    private static final String QUOTE = "\"";
+    private static final String NULL_STR = "";
+    private static final String COMMA = ",";
 
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab3 Spark App");
@@ -20,6 +27,20 @@ public class SparkApp {
                 textFile("L_AIRPORT_ID.csv");
 
         JavaPairRDD<Integer, String> dataAirport = airportsFile.
-                filter(s -> !s.contains())
+                filter(s -> !s.contains(CODE)).
+                mapToPair(s -> {
+                            s = s.replace(QUOTE, NULL_STR);
+                            int firstCommaIndex = s.indexOf(COMMA);
+                            return new Tuple2<>(
+                                    Integer.valueOf(
+                                            s.substring(0, firstCommaIndex)
+                                    ),
+                                    s.substring(firstCommaIndex + 1, s.length())
+                            );
+                        }
+                );
+        final Broadcast
+                <Map<Integer, String
+                        >
     }
 }
