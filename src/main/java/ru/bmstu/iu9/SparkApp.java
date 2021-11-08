a74fecc3a63755409f05739e268fbacaf841304b
DionisSerbin <reaganusm@gmail.com>
2021-11-08 11:52:06 +0300

    Scripted auto-commit on change (2021-11-08 11:52:06) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/SparkApp.java
+++ src/main/java/ru/bmstu/iu9/SparkApp.java
@@ -38,6 +38,7 @@ public class SparkApp {
     }
 
     public static void main(String[] args){
+        
         SparkConf conf = new SparkConf().setAppName("lab3 Spark App");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
